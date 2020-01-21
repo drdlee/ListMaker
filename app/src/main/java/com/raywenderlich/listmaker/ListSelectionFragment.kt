d4e2f6aebc123eeb5244742b8db95b3a334f2fb6
drdlee <danielrudi.lee@gmail.com>
2020-01-21 17:35:25 +0800

    Init Fragment for MainActivity

---
+++ app/src/main/java/com/raywenderlich/listmaker/ListSelectionFragment.kt
@@ -0,0 +1,51 @@
+package com.raywenderlich.listmaker
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ListSelectionFragment : Fragment() {
+
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_selection, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(list: TaskList)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): ListSelectionFragment {
+            return ListSelectionFragment()
+        }
+    }
+}

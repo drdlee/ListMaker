8a26e5e4a8d41870b8b5b40d3f8815f26d26f7ae
drdlee <danielrudi.lee@gmail.com>
2020-01-21 12:20:48 +0800

    use RecyclerView in ListDetailActivity

--- app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
@@ -1,11 +1,15 @@
 package com.raywenderlich.listmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListDetailActivity : AppCompatActivity() {
 
     lateinit var list: TaskList
+    lateinit var listItemsRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +17,9 @@ class ListDetailActivity : AppCompatActivity() {
 
         list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)
         title = list.name
+
+        listItemsRecyclerView = findViewById(R.id.list_items_recyclerview)
+        listItemsRecyclerView.adapter = ListItemsRecyclerViewAdapter(list)
+        listItemsRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
